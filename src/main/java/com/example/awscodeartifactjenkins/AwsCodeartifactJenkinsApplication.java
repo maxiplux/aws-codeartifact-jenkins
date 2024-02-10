bc700a2f7fec352429d42c9a97fc41752c09c734
aek <anicetkeric@outlook.com>
2024-02-10 12:39:57 +0100

    initial commit

---
+++ src/main/java/com/example/awscodeartifactjenkins/AwsCodeartifactJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.awscodeartifactjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCodeartifactJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCodeartifactJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/awscodeartifactjenkins/AwsCodeartifactJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awscodeartifactjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCodeartifactJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
